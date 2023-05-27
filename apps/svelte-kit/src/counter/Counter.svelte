2c5e87c432aecc2c537af07e86169da3cd20edfd
Rafał Majewski <goodheropl@gmail.com>
2023-05-27 23:17:07 +0200

    Make a component in SvelteKit example

---
+++ apps/svelte-kit/src/counter/Counter.svelte
@@ -0,0 +1,26 @@
+<script>
+	import {onMount} from "svelte";
+
+	let counter = 0;
+	const incrementCounter = () => {
+		++counter;
+	};
+	let isMounted = false;
+	onMount(() => {
+		isMounted = true;
+	});
+	$: {
+		if (isMounted) alert(counter);
+	}
+</script>
+
+<div>
+	<h1 class="counter">Licznik: {counter}</h1>
+	<button on:click={incrementCounter}>Zwiększ</button>
+</div>
+
+<style>
+	.counter {
+		font-size: 30px;
+	}
+</style>
