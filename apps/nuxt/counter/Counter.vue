9b74d48b39b6256c274bb2dac3b16412ff15d994
Rafał Majewski <goodheropl@gmail.com>
2023-05-28 11:45:18 +0200

    Nuxt.js SSR app

---
+++ apps/nuxt/counter/Counter.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+	import {watch, ref} from "vue";
+
+	let counter = ref(0);
+	watch(counter, (newCounter) => {
+		alert(newCounter);
+	});
+	const incrementCounter = () => {
+		++counter.value;
+	};
+</script>
+
+<template>
+	<div>
+		<h1 class="counter">Licznik: {{ counter }}</h1>
+		<button @click="incrementCounter">Zwiększ</button>
+	</div>
+</template>
+
+<style scoped>
+	.counter {
+		font-size: 30px;
+	}
+</style>
